5f5cea8ac23e342f3080d833e082efe3f42faea2
arsh0007 <arshkumar213@gmail.com>
2023-10-09 16:52:25 +0530

    Contact section addition

---
+++ src/app/components/FooterSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const FooterSection = () => {
+  return (
+    <footer className="footer border border-top-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="flex container p-5 justify-between">
+        <span>
+            Software Developer
+        </span>
+        <p className="text-slate-600">All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default FooterSection
